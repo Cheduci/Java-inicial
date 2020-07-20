d526d0949ba8de914c7781ade8d8e13b6a778af5
Chino <matiastu-te_@hotmail.com>
2020-07-20 14:34:08 -0300

    se agruparon las líneas

--- src/Dec_variables2.java
+++ src/Dec_variables2.java
@@ -1,21 +1,14 @@
 public class Dec_variables2 {
     public static void main(String[] args){
         final float apulgadas = (float) 2.54;
-
         float cm = 6;
-
         float resultado = cm/apulgadas;
-
         System.out.println("En " + cm + "cm hay " + resultado + " pulgadas");
 
         int operador1, operador2, resultado2;
-
         operador1 = 8;
-
         operador2 = 7;
-
         resultado2 = operador1 + operador2;
-        
         System.out.println(resultado2);
 
     }
