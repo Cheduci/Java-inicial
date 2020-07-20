3b0048dcda784159cbc59f59940b1e5c75c344a2
Chino <matiastu-te_@hotmail.com>
2020-07-20 14:28:57 -0300

    practicando floats

---
+++ src/Dec_variables2.java
@@ -0,0 +1,13 @@
+public class Dec_variables2 {
+    public static void main(String[] args){
+        final float apulgadas = (float) 2.54;
+
+        float cm = 6;
+
+        float resultado = cm/apulgadas;
+
+        System.out.println("En " + cm + "cm hay " + resultado + " pulgadas");
+
+    }
+    
+}
