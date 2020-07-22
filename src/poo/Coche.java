9b24743288b3658c4a72ed51feb412d9782ec00b
Chino <matiastu-te_@hotmail.com>
2020-07-22 12:02:51 -0300

    practicando getter, setter y setter+getter

--- src/poo/Coche.java
+++ src/poo/Coche.java
@@ -5,11 +5,8 @@ public class Coche {
     Se recomienda usar mayúscula al definir una clase
     */
     private int ruedas; // agregando "private" adelante, se encapsula la propiedad
-    private int largo;
-    private int ancho;
-    private int motor;
-    private int peso;
-    String color;
+    private int largo, ancho, motor, peso, peso_plataforma;
+    private String color;
     int peso_total;
     boolean asientos_cuero, climatizador;
 
@@ -21,32 +18,85 @@ public class Coche {
         motor = 1600;
         peso = 500;
         color = "rojo";
+        peso_plataforma = 500;
     }
 
-    public String dime_ruedas() {
-        return "El auto tiene " + ruedas + " ruedas";
+    public String get_estado() {
+        return "El auto tiene " + ruedas + " ruedas, mide " + largo + " cm de largo, " + ancho + " cm de ancho, tiene " + motor + " ml de cilindrada, pesa " + peso + " kg.";
     }
     /* esto es un Getter, permite obtener información de una propiedad encapsulada
     Debe llevar un return siempre */
-    public String dime_largo() {
-        return "El largo del coche es " + largo + "cm";
-    }
-    public String dime_ancho() {
-        return "El ancho del coche es " + ancho + "cm";
-    }
-    public String dime_motor() {
-        return "El motor del coche tiene " + motor + "ml de cilindrada";
-    }
-    public String dime_peso() {
-        return "El peso del pesa " + peso + "kg.";
-    }
 
     public void set_color(String color_Coche){
         color = color_Coche;
     }
     /* Esto es un Setter, permite asignar un valor a una propiedad
     al llevar "void" indica que no devuelve nada*/
-    public String dime_color(){
+    public String get_color(){
         return "El color del coche es " + color;
+    }
+    
+    public void set_asientos(String asientos_cuero){ // setter
+        if (asientos_cuero == "si") {
+            this.asientos_cuero = true;
+        }
+        else{
+            this.asientos_cuero = false;
+        }
+    }
+    public String get_asientos(){ // getter
+        if (asientos_cuero == true){
+            return "El coche tiene asientos de cuero";
+        }
+        else{
+            return "El coche tiene asientos de serie";
+        }
+    }
+
+    public void set_climatizador(String climatizador){ // setter
+        if (climatizador == "si"){
+            this.climatizador = true;
+        }
+        else{
+            this.climatizador = false;
+        }
+    }
+    public String get_climatizador(){ // getter
+        if (climatizador == true){
+            return "El coche tiene climatizador";
+        }
+        else{
+            return "El coche no tiene climatizador";
+        }
+    }
+
+    public String get_peso_coche(){ /* setter + getter
+        normalmente no se recomienda usarlo, pero se puede */
+        int peso_carroceria = 500;
+        peso_total = peso_plataforma + peso_carroceria;
+
+        if (asientos_cuero == true){
+            peso_total += 50;
+        }
+
+        if (climatizador == true){
+            peso_total += 20;
+        }
+        
+        return "El peso del coche es " + peso_total + " kg";
+    }
+
+    public int precio_coche(){ // getter
+        
+        int precio_final = 10000;
+
+        if (asientos_cuero == true){
+            precio_final += 2000;
+        }
+        if (climatizador == true){
+            precio_final += 1500;
+        }
+
+        return precio_final;
     }
 }
--- src/poo/Uso_coche.java
+++ src/poo/Uso_coche.java
@@ -4,11 +4,16 @@ public class Uso_coche {
     public static void main(String[] args) {
         
         Coche micoche = new Coche(); // Renault es una instancia del objeto Coche
-        /* El "Coche" el inicio de la línea es la clase declarada
+        /* El "Coche" del inicio de la línea es la clase declarada
         la palabra que sigue es el nombre de la variable
         */
         micoche.set_color("turquesa");
-        System.out.println(micoche.dime_color());
+
+        System.out.println(micoche.get_estado());
+        System.out.println(micoche.get_color());
+
+        micoche.set_asientos("si");
+        System.out.println(micoche.get_asientos());
     }
     
 }
